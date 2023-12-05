7b2b42df10f009390beb9f89f51fb5b9b0f0d7a4
Lucas Pinho <lucashpsfei@gmail.com>
2023-12-05 20:08:13 -0300

    :tada: feat: Implemented base of microservice.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lhps/code-invest-go
+
+go 1.21.3
+
+require github.com/google/uuid v1.4.0
---
+++ internal/market/entity/order_queue.go
@@ -0,0 +1,41 @@
+package entity
+
+type OrderQueue struct {
+	Orders []*Order
+}
+
+// Métodos
+//Less Comparar dois valores
+//Swap Inverte valores
+//Len Tamanho dos dados
+//Push Adicionar novos valores
+//Pop Remove um dado de uma posição
+
+func (oq OrderQueue) Less(i, j int) bool {
+	return oq.Orders[i].Price < oq.Orders[j].Price
+}
+
+func (oq OrderQueue) Swap(i, j int) {
+	oq.Orders[i], oq.Orders[j] = oq.Orders[j], oq.Orders[i]
+}
+
+func (oq OrderQueue) Len() int {
+	return len(oq.Orders)
+}
+
+func (oq OrderQueue) Push(x interface{}) {
+	oq.Orders = append(oq.Orders, x.(*Order))
+}
+
+func (oq OrderQueue) Pop() interface{} {
+	old := oq.Orders
+	n := len(old)
+	item := old[n-1]
+	oq.Orders = old[0 : n-1]
+
+	return item
+}
+
+func NewOrderQueue() *OrderQueue {
+	return &OrderQueue{Orders: []*Order{}}
+}
